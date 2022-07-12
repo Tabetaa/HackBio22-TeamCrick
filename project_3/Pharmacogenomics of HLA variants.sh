e67860bf63dfc7768eeb707d34c825a485893fb0
Oreoluwa Akande <69676553+Tabetaa@users.noreply.github.com>
2022-07-12 19:55:47 +0100

    Update Pharmacogenomics of HLA variants.sh

--- project_3/Pharmacogenomics of HLA variants.sh
+++ project_3/Pharmacogenomics of HLA variants.sh
@@ -25,7 +25,6 @@ gunzip asia.bed.gz
 plink —file asia —pca 
 
 #download file from linux terminal to Rstudio for further analysis
-
 #print working directory and copy
 #pwd
 #open local directory terminal and download file from server
@@ -40,33 +39,42 @@ pca1 <- read.table("/Users/project3/plink.eigenvec", sep= "", header = F)
 #check the first 5 lines of the file
 head(pca1)
 
+#preliminary plot using pca1
+library("ggplot2")
+ggplot(data=pca1, aes(V3,V4)) + geom_point()
+
 #load 1000_genome_sample list
 metadata <- read.table("/Users/project3/complete_1000_genomes_sample_list_.tsv", sep= "\t", header = TRUE)
 
 #merge pca1 and metadata data by using the common column in both samples
 merge_data <- merge(x= pca1, y= metadata, by.x = "V2", by.y = "Sample.name", all = F)
 
-#load ggplot to do the PCA plot
+#load ggplot to do the PCA plot, change title and labels 
 PCA <- ggplot(data=merge_data, aes(V3,V4, color = Population.code)) + geom_point()
+          +xlab("Principal Component 1 (PC1)")
+          +ylab("Principal Component 2 (PC2)")
+          +ggtitle("PCA of selected Asain Populations")
 
-#change x and y labels and title of plot
-print(PCA ggplot(t + labs( x = "PCA1", y = "PCA2"))
-print(PCA + ggtitle("PCA Plot Of Asian Population"))
 
-
-#Linkage disequilibrium analysis
+#Multidimensional Scaling Analysis
 #Create a pruned set of markers that are not highly correlated
 
-plink --bfile asia --indep-pairwise 100 5 0.01— out prune1
+plink --bfile asia --indep-pairwise 1000 10 0.01— out prune1
 
 #Calculate identity by descent score on the pruned marker list
 plink bfile —extract prune1.prunein —genome —out ibs1
 
-#Extract a subset of SNP based on physical location of chromosome
-plink --bfile asia --chr 8 --from-kb 220 --to-kb 146300 make-bed  --out asia_chrom8
-plink --bfile asia --chr 6 --from-kb 232 --to-kb 171032 --make-bed --out asia_chrom6
-plink --bfile asia --chr 15 --from-kb 20108 --to-kb 102321 --make-bed --out asia_chrom15
+#POPULATION STRATIFICATION
+plink --bfile asia --read-genome ibs1.genome --cluster --ppc 1e-3 --cc --mds-plot 2 --out strat1
 
+#MDS VISUALISATION
+mdsdata <- read.table("strat1.mds", header = TRUE)
+merged_mds <- merge(x=msdata, y=metadata, by.x= "FID", by.y= "Sample.name", all = F)
+
+#Scatterplot 
+
+ggplot(data=merged_mds, aes(C1,C2, color = Population.code)) + geom_point()
+        +ggtitle("Multidimensional Scaling Analysis")
 
 #download link for R & R-studio(macOS)
 #https://cran.r-project.org/bin/macosx/
